8854338dc99b0c16c53c0f8dd8f3d0366febc19c
Elizabeth Tenorio <betsky98@gmail.com>
2013-04-09 01:41:11 -0400

    Add basic app structure

---
+++ group_app/base_app.py
@@ -0,0 +1,36 @@
+# Group web app
+# Name: Beth and Tracy
+
+import flask
+import settings
+
+#Views
+from login import Login
+from main import Main
+from gallery import Gallery
+
+app = flask.Flask(__name__)
+app.secret_key = settings.secret_key
+
+
+#Routes
+app.add_url_rule('/',
+                view_func=Main.as_view('main'),
+                methods=['GET'])
+app.add_url_rule('/<page>/',
+                view_func=Main.as_view('main'),
+                methods=['GET'])
+app.add_url_rule('/gallery/',
+                view_func=Gallery.as_view('gallery'),
+                methods=['GET'])
+app.add_url_rule('/login/',
+                view_func=Login.as_view('login'),
+                methods=['GET', 'POST'])
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+     return flask.render_template('404.html'), 404
+
+app.debug = True
+app.run()
